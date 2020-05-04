c42fd4f98b1f4f0d9707888dbe4960064acc77c3
marc <jones.marc@gmail.com>
2020-05-04 20:14:56 +0100

    quoted

---
+++ Lisp.Compiler/Unquoted.cs
@@ -0,0 +1,14 @@
+namespace Lisp.Compiler
+{
+	public class Unquoted : IStringify
+	{
+		public object Value { get; }
+
+		public Unquoted(object value)
+		{
+			Value = value;
+		}
+		
+		public string Stringify(bool quoteStrings) => "~" + Value.Stringify();
+	}
+}
