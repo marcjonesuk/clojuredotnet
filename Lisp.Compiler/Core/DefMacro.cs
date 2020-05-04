02be720a284fef194ce02211e39bc07104ec680d
marc <jones.marc@gmail.com>
2020-05-04 20:16:40 +0100

    keyword

--- Lisp.Compiler/Core/DefMacro.cs
+++ Lisp.Compiler/Core/DefMacro.cs
@@ -1,26 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
+// using System;
+// using System.Collections.Generic;
+// using System.Collections.Immutable;
+// using System.Linq;
 
-namespace Lisp.Compiler
-{
-    public class DefMacro : IFn
-    {
-        public object Invoke(object[] args)
-        {
-            var symbol = args[0] as Symbol;
-            var macroArgs = args[1].As<IEnumerable<object>>();
-            var macro = args[2] as SymbolicExpression;
+// namespace Lisp.Compiler
+// {
+//     public class DefMacro : IFn
+//     {
+//         public object Invoke(object[] args)
+//         {
+//             var symbol = args[0] as Symbol;
+//             var macroArgs = args[1].As<IEnumerable<object>>();
+//             var macro = args[2] as SymbolicExpression;
 
-            var fn = new Macro(macroArgs.Select(a => ((Symbol)a).Name), a =>
-            {
-                var x = macro.Invoke(null);
-                return x;
-            });
+//             var fn = new Macro(macroArgs.Select(a => ((Symbol)a).Name), a =>
+//             {
+//                 var x = macro.Invoke(null);
+//                 return x;
+//             });
 
-            Environment.Current.Macros.Add(symbol.Name, fn);
-            return symbol;
-        }
-    }
-}
+//             Environment.Current.Macros.Add(symbol.Name, fn);
+//             return symbol;
+//         }
+//     }
+// }
