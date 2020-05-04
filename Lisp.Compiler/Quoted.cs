7036d4160adfa69c00b1ba87bd3f703af84beb2a
marc <jones.marc@gmail.com>
2020-05-04 20:13:52 +0100

    quoted

---
+++ Lisp.Compiler/Quoted.cs
@@ -0,0 +1,14 @@
+namespace Lisp.Compiler
+{
+	public class Quoted : IStringify
+	{
+		public object Value { get; }
+
+		public Quoted(object value)
+		{
+			Value = value;
+		}
+
+		public string Stringify(bool quoteStrings) => "`" + Value.Stringify(quoteStrings);
+	}
+}
